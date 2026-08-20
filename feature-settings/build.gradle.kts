1105d000c38b24e5ee08c11bcc46047b1ff5b511
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-20 12:05:12 +0530

    build: add Compose foundation for Lens Manager

--- feature-settings/build.gradle.kts
+++ feature-settings/build.gradle.kts
@@ -27,5 +27,6 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
 }
