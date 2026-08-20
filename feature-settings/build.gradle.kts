a7c0db56bcea21d35b5be5e4844782947f75fff7
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-20 11:59:50 +0530

    build: add explicit coroutines dependency

--- feature-settings/build.gradle.kts
+++ feature-settings/build.gradle.kts
@@ -24,6 +24,7 @@ android {
 dependencies {
     implementation(project(":core-model"))
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.kotlinx.coroutines.android)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
